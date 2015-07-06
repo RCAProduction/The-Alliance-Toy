218e7562af37254354b614d5de9fa8887075c0a0
RCAProduction <rcaproductionmaster@gmail.com>
2015-07-06 12:15:07 -0600

    Added tmp3 and tmp4.

--- src/simulation/Particle.h
+++ src/simulation/Particle.h
@@ -14,6 +14,8 @@ struct Particle
 	int flags;
 	int tmp;
 	int tmp2;
+	int tmp3;
+	int tmp4;
 	unsigned int dcolour;
 	/** Returns a list of properties, their type and offset within the structure that can be changed
 	 by higher-level processes referring to them by name such as Lua or the property tool **/
