3458716fe8526c28eb3d65134ea94b7175c939f6
RCAProduction <rcaproductionmaster@gmail.com>
2015-07-09 20:42:09 -0600

    Blank bots will assume other types. Fixed spacing. Replicators work.

--- src/simulation/elements/NBOT.cpp
+++ src/simulation/elements/NBOT.cpp
@@ -52,12 +52,13 @@ int Element_NBOT::update(UPDATE_FUNC_ARGS)
 
 int r, rx, ry, ct = parts[i].ctype, giveAmount, t, tx, ty, multiplyVal, ovx, ovy;
 
-if(ct==PT_NBOT)
+if (ct==PT_NBOT)
 {
-	ct = PT_NONE;
+	parts[i].ctype = 0;
 }
-if(parts[i].temp>=1825.15)
-	if(parts[i].tmp==1)
+
+if (parts[i].temp>=1825.15)
+	if (parts[i].tmp==1)
 	{
 		parts[i].temp = 1800;
 		parts[i].life = parts[i].life - 2;
@@ -66,60 +67,65 @@ if(parts[i].temp>=1825.15)
 	{
 	sim->part_change_type(i,x,y,PT_BREC);
 	}
-if(parts[i].tmp2==2)
+	
+if (parts[i].tmp2==2)
 {
 	parts[i].life = 255;
 	parts[i].tmp2 = 3;
 }
-if(parts[i].life<=0 && parts[i].tmp2!=0)
+
+if (parts[i].life<=0 && parts[i].tmp2!=0)
 {
 	parts[i].tmp2 = 4;
 }
-if(parts[i].life>=1 && parts[i].tmp2==4)
+
+if (parts[i].life>=1 && parts[i].tmp2==4)
 	parts[i].tmp2 = 3;
-if((parts[i].life>=1 && parts[i].tmp2==1) || (parts[i].life>=1 && parts[i].tmp2==3)) //Movement when active
+	
+if ((parts[i].life>=1 && parts[i].tmp2==1) || (parts[i].life>=1 && parts[i].tmp2==3)) //Movement when active
 {
-	if(parts[i].tmp==3 && parts[i].tmp3>=1)
+	if (parts[i].tmp==3 && parts[i].tmp3>=1)
 	{
-		if(parts[i].x>parts[i].tmp3)
+		if (parts[i].x>parts[i].tmp3)
 		{
 			ovx = (rand() % 21)-16;
 		}
-		else if(parts[i].x<parts[i].tmp3)
+		else if (parts[i].x<parts[i].tmp3)
 		{
 			ovx = (rand() % 21)-4;
 		}
-		if(parts[i].y>parts[i].tmp4)
+		if (parts[i].y>parts[i].tmp4)
 		{
 			ovy = (rand() % 9)-7;
 		}
-		else if(parts[i].y<parts[i].tmp4)
+		else if (parts[i].y<parts[i].tmp4)
 		{
 			ovy = (rand() % 9)-1;
 		}
 	}
-	else if(parts[i].tmp3>=1 && parts[i].x>parts[i].tmp3 && parts[i].tmp!=6)
+	else if (parts[i].tmp3>=1 && parts[i].x>parts[i].tmp3 && parts[i].tmp!=6)
 	{
 		ovx = (rand() % 11)-8;
 	}
-	else if(parts[i].tmp3>=1 && parts[i].x<parts[i].tmp3 && parts[i].tmp!=6)
+	else if (parts[i].tmp3>=1 && parts[i].x<parts[i].tmp3 && parts[i].tmp!=6)
 	{
 		ovx = (rand() % 11)-2;
 	}
-	if(parts[i].tmp4>=1 && parts[i].y>parts[i].tmp4 && parts[i].tmp!=6)
+	
+	if (parts[i].tmp4>=1 && parts[i].y>parts[i].tmp4 && parts[i].tmp!=6)
 	{
 		ovy = (rand() % 11)-8;
 	}
-	else if(parts[i].tmp4>=1 && parts[i].y<parts[i].tmp4 && parts[i].tmp!=6)
+	else if (parts[i].tmp4>=1 && parts[i].y<parts[i].tmp4 && parts[i].tmp!=6)
 	{
 		ovy = (rand() % 11)-2;
 	}
-	else if(parts[i].tmp==3 && parts[i].tmp3==0)
+	else if (parts[i].tmp==3 && parts[i].tmp3==0)
 	{
 		ovx = (rand() % 21)-10;
 		ovy = (rand() % 9)-4;
 	}
-	else if(parts[i].tmp3==0)
+	else if (parts[i].tmp3==0)
 	{
 		ovx = (rand() % 11)-5;
 		ovy = (rand() % 11)-5;
@@ -128,22 +134,28 @@ if((parts[i].life>=1 && parts[i].tmp2==1) || (parts[i].life>=1 && parts[i].tmp2=
 	parts[i].vy = ovy;
 }
 
-if(parts[i].tmp3>0 || parts[i].tmp4>0)
+if (parts[i].tmp3>0 || parts[i].tmp4>0)
 {
 	parts[i].tmp3 = 0;
 	parts[i].tmp4 = 0;
 }
 
-if(parts[i].tmp==1 && parts[i].life<=10 && parts[i].life>=1)
+if (parts[i].tmp==0 && parts[i].ctype==0 && parts[i].tmp2==0 && parts[i].pavg[0]<100)
+{
+	parts[i].pavg[0] = parts[i].pavg[0] + 1;
+}
+
+if (parts[i].tmp==1 && parts[i].life<=10 && parts[i].life>=1)
 {
 	parts[i].tmp = 1;
 	parts[i].life = 1;
 	sim->part_change_type(i,x,y,PT_FIRW);
 }
 
-if(parts[i].life<=0)
+if (parts[i].life<=0)
 	parts[i].life = 0;
-if(parts[i].life>2000 && parts[i].tmp!=2)
+	
+if (parts[i].life>2000 && parts[i].tmp!=2)
 	parts[i].life = 2000;
 	
 	for (rx=-1; rx<2; rx++)
@@ -154,9 +166,9 @@ if(parts[i].life>2000 && parts[i].tmp!=2)
 				if (!r)
 					continue;
 				
-				if(parts[r>>8].life<parts[i].life && parts[r>>8].type==PT_NBOT && parts[i].tmp2>=1 && parts[i].tmp2<=4 && parts[r>>8].tmp2>=1 && parts[r>>8].tmp2<=4)
+				if (parts[r>>8].life<parts[i].life && parts[r>>8].type==PT_NBOT && parts[i].tmp2>=1 && parts[i].tmp2<=4 && parts[r>>8].tmp2>=1 && parts[r>>8].tmp2<=4)
 				{
-					if(parts[i].life>255 && parts[i].tmp==2)
+					if (parts[i].life>255 && parts[i].tmp==2)
 					{
 						giveAmount = 255 - parts[r>>8].life;
 						parts[r>>8].life = parts[r>>8].life + giveAmount;
@@ -169,89 +181,83 @@ if(parts[i].life>2000 && parts[i].tmp!=2)
 						parts[i].life = parts[i].life - giveAmount;
 					}
 				}
-				if((parts[r>>8].type==PT_SPRK && parts[r>>8].ctype==PT_PSCN) || (parts[r>>8].type==PT_VSPK && parts[r>>8].ctype==PT_PSCN))
+				
+				if ((parts[r>>8].type==PT_SPRK && parts[r>>8].ctype==PT_PSCN) || (parts[r>>8].type==PT_VSPK && parts[r>>8].ctype==PT_PSCN))
 				{
 					parts[i].life = 255;
 					parts[i].tmp2 = 1; //Set bot as active from PSCN
 				}
-				if((parts[r>>8].type==PT_NBOT && parts[i].tmp2==1 && parts[r>>8].tmp2==0) || (parts[r>>8].type==PT_NBOT && parts[i].tmp2==2 && parts[r>>8].tmp2==0)) //Activates touching bots
+				
+				if ((parts[r>>8].type==PT_NBOT && parts[i].tmp2==1 && parts[r>>8].tmp2==0) || (parts[r>>8].type==PT_NBOT && parts[i].tmp2==2 && parts[r>>8].tmp2==0)) //Activates touching bots
 				{
 					parts[r>>8].tmp2 = 2;
 				}
 
-				if(parts[i].tmp==0 && parts[r>>8].type==ct && parts[i].tmp2>=1 && parts[i].tmp2<=3 && parts[r>>8].tmp<=1000) //Check for retrieve flag. It is the default, 0
+				if (parts[i].tmp==0 && parts[r>>8].type==ct && parts[i].tmp2>=1 && parts[i].tmp2<=3 && parts[r>>8].tmp<=1000) //Check for retrieve flag. It is the default, 0
 				{
 					parts[i].tmp = 256; //Set as full
 					sim->kill_part(r>>8);
 				}
-				if(parts[i].tmp==1)
+				
+				if (parts[i].tmp==1)
 				{
-					if(parts[i].life>=1 && parts[i].tmp==1 && ct==parts[r>>8].type)
+					if (parts[i].life>=1 && parts[i].tmp==1 && ct==parts[r>>8].type)
 					{
 						parts[i].tmp = 1;
 						parts[i].life = 1;
 						sim->part_change_type(i,x,y,PT_FIRW);
 					}
 				}
-				if(parts[i].tmp==2 && parts[i].tmp2>=1 && parts[i].tmp2<=3 && parts[r>>8].type==ct && parts[i].life<7000) //Chargers
+				
+				if (parts[i].tmp==2 && parts[i].tmp2>=1 && parts[i].tmp2<=3 && parts[r>>8].type==ct && parts[i].life<7000) //Chargers
 				{
 					parts[i].life = parts[i].life + ((rand() % 100)+1000);
 					sim->kill_part(r>>8);
 				}
-				if(parts[i].tmp==4 && parts[i].life>=1 && parts[r>>8].type==ct) //Break flag
+				
+				if (parts[i].tmp==4 && parts[i].life>=1 && parts[r>>8].type==ct) //Break flag
 				{
 					sim->kill_part(r>>8);
 					parts[i].vx = (rand() % 17)-8;
 					parts[i].vy = (rand() % 17)-8;
 					sim->part_change_type(i,x,y,PT_BREC);
 				}
-				if((parts[i].tmp==5 && parts[i].life>=1 && parts[r>>8].type==PT_BREC) || (parts[i].tmp==12 && parts[i].life>=1)) //Replicate flag
+				
+				if (parts[i].tmp==5 && parts[i].life>=1 && parts[r>>8].type==PT_BREC) //Replicate flag
 				{
-					if(/*parts[i].tmp==257 && parts[r>>8].type==PT_NBOT*/1==1)
-					{
-						//parts[i].ctype = (parts[r>>8].tmp^10)+parts[r>>8].ctype;
-						for (tx=-1; tx<2; tx++)
-							for (ty=-1; ty<2; ty++)
-								if (BOUNDS_CHECK && (tx || ty))
+					for (tx=-1; tx<2; tx++)
+						for (ty=-1; ty<2; ty++)
+							if (BOUNDS_CHECK && (tx || ty))
+							{
+								t = pmap[y+ty][x+tx];
+								
+								if (!t)
 								{
-									t = pmap[y+ty][x+tx];
-									
-									if(!t)
-									{
-										sim->create_part(-1,x+tx,y+ty,PT_NBOT);
-										t = pmap[y+ty][x+tx];
-										parts[t].tmp2 = 4;
-										parts[t].tmp = 1;
-										parts[i].tmp = 5;
-									}
-
+									sim->create_part(-1,x+tx,y+ty,PT_NBOT);
 								}
-							return 0;
-					}
-					if(parts[i].tmp!=257)
-					{
-						parts[i].tmp = 257;
-						sim->kill_part(r>>8);
-					}
-				}
-				if(parts[i].tmp==6 && parts[i].life>=1 && parts[r>>8].type==ct)
-				{
-						for (rx=-20; rx<21; rx++)
-							for (ry=-20; ry<21; ry++)
-								if (BOUNDS_CHECK && (rx || ry))
-								{
-									r = pmap[y+ry][x+rx];
-									if (!r)
-										continue;
-									if(parts[r>>8].type==PT_NBOT && parts[r>>8].tmp!=6)
-									{
-										parts[r>>8].tmp3 = parts[i].x;
-										parts[r>>8].tmp4 = parts[i].y;
-									}
-								}
+							}
 						return 0;
 				}
-				if(parts[i].tmp==6 && parts[i].life==0)
+				
+				if (parts[i].tmp==6 && parts[i].life>=1 && parts[r>>8].type==ct)
+				{
+					for (rx=-20; rx<21; rx++)
+						for (ry=-20; ry<21; ry++)
+							if (BOUNDS_CHECK && (rx || ry))
+							{
+								r = pmap[y+ry][x+rx];
+								if (!r)
+									continue;
+								if (parts[r>>8].type==PT_NBOT && parts[r>>8].tmp!=6)
+								{
+									parts[r>>8].tmp3 = parts[i].x;
+									parts[r>>8].tmp4 = parts[i].y;
+								}
+							}
+					return 0;
+				}
+				
+				if (parts[i].tmp==6 && parts[i].life==0)
 				{
 						for (rx=-20; rx<21; rx++)
 							for (ry=-20; ry<21; ry++)
@@ -268,11 +274,11 @@ if(parts[i].life>2000 && parts[i].tmp!=2)
 						return 0;
 				}
 
-				if((parts[r>>8].type==PT_SPRK && parts[r>>8].ctype==PT_NSCN && parts[i].tmp==256) || (parts[r>>8].type==PT_VSPK && parts[r>>8].ctype==PT_NSCN && parts[i].tmp==256)) //If NSCN is sparked, drop element
+				if ((parts[r>>8].type==PT_SPRK && parts[r>>8].ctype==PT_NSCN && parts[i].tmp==256) || (parts[r>>8].type==PT_VSPK && parts[r>>8].ctype==PT_NSCN && parts[i].tmp==256)) //If NSCN is sparked, drop element
 				{
 					parts[i].vx = 0;
 					parts[i].vy = 0;
-					if(ct==PT_QRTZ || ct==PT_PQRT)
+					if (ct==PT_QRTZ || ct==PT_PQRT)
 					{
 						parts[i].tmp2 = 5;
 						parts[i].tmp=1000;
@@ -284,6 +290,14 @@ if(parts[i].life>2000 && parts[i].tmp!=2)
 					}
 					sim->part_change_type(i,x,y,ct);
 					ct = PT_NONE;
+				}
+				
+				if (parts[i].tmp==0 && parts[i].tmp2==0 && parts[i].pavg[0]>=100 && parts[r>>8].type==PT_NBOT && parts[r>>8].ctype!=0) //Allows blank NBOT to copy into other types
+				{
+					parts[i].tmp = parts[r>>8].tmp;
+					parts[i].tmp2 = parts[r>>8].tmp2;
+					parts[i].ctype = parts[r>>8].ctype;
+					parts[i].pavg[0] = 0;
 				}
 			}	
 	return 0;
