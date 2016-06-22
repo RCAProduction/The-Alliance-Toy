25a44b9091a15756e8972f4c7da4061de87ae21d
RCAProduction <rcaproductionmaster@gmail.com>
2016-06-22 02:00:24 -0600

    Added FILT values.

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -2442,9 +2442,11 @@ void GameView::OnDraw()
 		g->drawtext(240, 25-scrolling, "NEWS, HELP, INFO, AND REFERENCE GUIDE", 255, 255, 255, 255);
 		g->draw_line(15, 37-scrolling, 612, 37-scrolling, 255, 255, 0, 255);
 
-		g->drawtext(15, 49-scrolling, "LIST OF MOD KEY COMMANDS:\n\n'ctrl+h' opens the entrance screen\n'ctrl+shift+h' turns on Super Debug Mode\n'shift+n' opens this message\n'ctrl+p' opens a parts graph\n'ctrl+m' turns on 'Realistic Mode'\n", 200, 200, 200, 255);
+		g->drawtext(15, 49-scrolling, "LIST OF MOD KEY COMMANDS:\n\n'ctrl+h' opens the entrance screen\n'ctrl+shift+h' turns on Super Debug Mode\n'shift+n' opens this message\n'ctrl+p' opens a parts graph\n'ctrl+m' turns on 'Realistic Mode'\n", 200, 200, 200, 255); //ln 3-10
 
-		g->drawtext(15, 145-scrolling, " EXTRA ELEMENT INFORMATION:\n\nNBOT: Uses several modes, set by TMP. Once activated by PSCN and charged, do what they are set to.\n 0: Retrieve Particle\n 1: Explode\n 2: Charge\n 3: Fight\n 4: Break\n 5: Replicate\n 6: Beacon", 200, 200, 200, 255);
+		g->drawtext(15, 145-scrolling, " EXTRA ELEMENT INFORMATION:\n\nNBOT: Uses several modes, set by TMP. Once activated by PSCN and charged, do what they are set to.\n 0: Retrieve Particle\n 1: Explode\n 2: Charge\n 3: Fight\n 4: Break\n 5: Replicate\n 6: Beacon", 200, 200, 200, 255); //ln 11-20
+
+		g->drawtext(15, 289-scrolling, "FILT: This table shows basic ctype values for electronics.\n 1\n 2\n 4\n 8\n 16\n 32\n 64\n 128\n 256\n 512\n 1024\n 2048\n 4096\n 8192\n 16384\n 32768\n 65536\n 131072\n 262144\n 524288\n 1048576\n 2097152\n 4194304\n 8388608\n 16777216\n 33554432\n 67108864\n 134217728\n 268435456\n 536870912", 200, 200, 200, 255); //ln 22-53
 
 		g->drawtext(15, 4692-scrolling, "This is the last line.", 255, 0, 0, 255);
 
